c3dab82cad8be2c46f8074c4bd370bc2c94b2a2d
justkingsley <kingsleybingala@gmail.com>
2023-06-19 15:23:04 +0200

    Completed header

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,8 @@ export default function Home() {
   return (
     <div>
       <Head>
+        <title>React Forms Tutorial</title>
+        <meta name = "description" content="React Forms Tutorial"/>
         
       </Head>
     </div>
