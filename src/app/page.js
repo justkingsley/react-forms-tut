2da34d1c52b6ed4e103d27cf605aaab7ee4c8846
justkingsley <kingsleybingala@gmail.com>
2023-06-20 13:28:42 +0200

    Selection drop down set.

--- src/app/page.js
+++ src/app/page.js
@@ -1,38 +1,52 @@
 import FormComponent from "@/components/FormComponent";
 import Head from "next/head";
 
+
 export default function Home() {
   return (
     <div>
       <Head>
         <title>React Forms Tutorial</title>
-        <meta name = "description" content="React Forms Tutorial"/>
+        <meta name="description" content="React Forms Tutorial" />
       </Head>
 
-      <main className = "h-screen flex items-center justify-center">
-        <form className = "bg-white flex rounded-lg w-1/2 font-latoRegular">
-          <div className = "flex-1 text-gray-700 p-20">
-            <h1 className = "text-3xl pb-2 font-latoRegular">Hello there 👋🏿</h1>
+      <main className="h-screen flex items-center justify-center">
+        <form className="bg-white flex rounded-lg w-1/2 font-latoRegular">
+          <div className="flex-1 text-gray-700 p-20">
+            <h1 className="text-3xl pb-2 font-latoRegular">Hello there 👋🏿</h1>
             <p>Sign up to start shipping.</p>
 
 
-            <div className = "mt-6">
-              <FormComponent 
+            <div className="mt-6">
+              <FormComponent
                 labelName={'Name'}
                 type={'text'}
                 htmlFor={'name'}
-                id={'id'}
-                placeholder={'Fill in your name'}
+                id={'name'}
+                placeholder={'Enter your name'}
               />
 
 
-              <FormComponent 
-                labelName={'Name'}
-                type={'text'}
-                htmlFor={'name'}
-                id={'id'}
-                placeholder={'Fill in your name'}
+              <FormComponent
+                labelName={'Email'}
+                type={'email'}
+                htmlFor={'email'}
+                id={'email'}
+                placeholder={'Enter your email'}
               />
+
+              <div className="pb-4">
+                <label
+                  htmlFor= 'city'
+                  className="block text-sm pb-2 font-latoBold text-gray-700">Select City</label>
+                <select className="border-2 text-sm focus:border-green-600 outline-none border-gray-500 p-2 rounded-md w-1/2">
+                  <option>Harare</option>
+                  <option>Bulawayo</option>
+                  <option>Gweru</option>
+                  <option>Mutare</option>
+                  <option>victoria Falls</option>
+                </select>
+              </div>
             </div>
 
 
