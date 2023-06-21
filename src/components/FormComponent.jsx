6beb9cb26222c113983afe79372d2618d8c3f22a
justkingsley <kingsleybingala@gmail.com>
2023-06-21 11:17:41 +0200

    Updated labels.

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const FormComponent = ({id, placeholder, type}) => {
+const FormComponent = ({id, placeholder, type, htmlFor, label}) => {
   return (
     <div className = "pb-4">
+              <label htmlFor = {htmlFor} className="text-sm text-gray-500 block">
+                {label}
+              </label>
               <input
               placeholder = {placeholder} id = {id} type = {type}
-              className = "border-1 text-sm placeholder-gray-400 focus:ring-0 focus:border-green-600 outline-none border-gray-500 p-2 rounded-md w-1/2"
+              className = "border-2 text-sm placeholder-gray-300 focus:ring-0 focus:border-green-600 outline-none border-gray-500 p-2 rounded-md w-1/2"
               />
     </div>
   )
