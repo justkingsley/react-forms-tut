e353e848a1a0ff870bd8bbc21ac8725a866cfdb9
justkingsley <kingsleybingala@gmail.com>
2023-06-21 10:43:18 +0200

    Removed label, will use placeholder instead.

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 
-const FormComponent = ({id, placeholder, type, htmlFor, labelName}) => {
+const FormComponent = ({id, placeholder, type}) => {
   return (
     <div className = "pb-4">
-              <label 
-              htmlFor = {htmlFor} 
-              className="block text-sm pb-2 font-latoBold text-gray-700">{labelName}</label>
               <input
               placeholder = {placeholder} id = {id} type = {type}
-              className = "border-2 text-sm focus:border-green-600 outline-none border-gray-500 p-2 rounded-md w-1/2"
+              className = "border-1 text-sm placeholder-gray-400 focus:ring-0 focus:border-green-600 outline-none border-gray-500 p-2 rounded-md w-1/2"
               />
     </div>
   )
