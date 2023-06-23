921e52b765fb0d226b2276111b8949f5e3dce8be
justkingsley <kingsleybingala@gmail.com>
2023-06-23 16:01:37 +0200

    Handling error display and color display.

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
-const FormComponent = ({id, placeholder, type, htmlFor, label, onChange, value}) => {
+const FormComponent = ({id, placeholder, type, htmlFor, label, onChange, value, error, onBlur, touchName}) => {
   return (
     <div className = "pb-4">
-              <label htmlFor = {htmlFor} className="text-sm text-gray-500 block">
-                {label}
+              <label htmlFor = {htmlFor} className={`text-sm text-gray-500 block ${error && touchName ? 'text-red-400 text-xs' : 'text-gray-500'}`}>
+
+                {touchName && error ? error : label} 
+
               </label>
               <input
-              placeholder = {placeholder} id = {id} type = {type} onChange = {onChange} value = {value}
+              placeholder = {placeholder} id = {id} type = {type} onChange = {onChange} value = {value} onBlur = {onBlur}
               className = "border-2 text-sm placeholder-gray-300 focus:ring-0 focus:border-green-600 outline-none border-gray-500 p-2 rounded-md w-full"
               />
     </div>
