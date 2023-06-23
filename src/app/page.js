97f8cc645d32b644c9776dacf0b7b137ad61daff
justkingsley <kingsleybingala@gmail.com>
2023-06-23 10:47:00 +0200

    Finally able to solve the sizing issue.

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,21 @@
+'use client'
 import FormComponent from "@/components/FormComponent";
 import Head from "next/head";
+import Lottie from 'lottie-react'
+import animationData from "@/app/delivery.json"
 
 
 export default function Home() {
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+        },
+      };
+
   return (
     <div>
       <Head>
@@ -12,9 +25,9 @@ export default function Home() {
 
       <main className="h-screen flex items-center justify-center">
         <form className="bg-white flex rounded-lg w-1/2 font-latoRegular">
-          <div className="flex-1 text-gray-700 p-20">
-            <h1 className="text-3xl pb-2 font-latoRegular">Hello there 👋🏿</h1>
-            <p>Sign up to start shipping.</p>
+          <div className="flex-1 text-gray-700 p-10">
+            <h1 className="text-4xl pb-2 font-latoRegular">Hello there 👋🏿</h1>
+            <p>Create your <span className="text-green-500">High Five</span> account.</p>
 
 
             <div className="mt-6">
@@ -39,7 +52,7 @@ export default function Home() {
                 label={'Phone'}
                 type={'phone'}
                 id={'phone'}
-                placeholder={'Enter your phone'}
+                placeholder={'Enter your phone number'}
               />
 
               <FormComponent
@@ -54,7 +67,7 @@ export default function Home() {
                 <label htmlFor='city' className="text-sm text-gray-500 block">
                   City
                 </label>
-                <select id='city' className="border-2 text-sm focus:ring-0 focus:border-green-600 outline-none border-gray-500 p-2 rounded-md w-1/2">
+                <select id='city' className="border-2 text-sm focus:ring-0 focus:border-green-600 outline-none border-gray-500 p-2 rounded-md w-full">
                   <option>Harare</option>
                   <option>Bulawayo</option>
                   <option>Gweru</option>
@@ -75,7 +88,7 @@ export default function Home() {
 
                 <div className="flex itmes-center gap-2">
                   <input type="checkbox" value="checked" name="terms" className="h-4 w-4 text-green-500 focus:border-green-600 hover:ring-green-500 focus:ring-0" />
-                  <p>I agree the <a href="#" className="text-blue-600 hover:underline">terms of service</a></p>
+                  <p>I agree the <a href="#" className="text-blue-600 hover:underline">terms of service</a> that your data may be sold for additional revenue.</p>
                 </div>
 
               </div>
@@ -91,6 +104,16 @@ export default function Home() {
 
 
           </div>
+
+          <div className="flex items-center h-50 w-50 p-5">
+            <Lottie
+              style={{ height: '600px', width: '400px' }}
+              options={defaultOptions}
+              animationData={animationData}
+            />
+          </div>
+
+
         </form>
       </main>
 
