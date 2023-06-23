4706962c747c0d247d8f38b80aa4f1db36601cf8
justkingsley <kingsleybingala@gmail.com>
2023-06-23 13:07:39 +0200

    Added new inputs

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const FormComponent = ({id, placeholder, type, htmlFor, label}) => {
+const FormComponent = ({id, placeholder, type, htmlFor, label, onChange, value}) => {
   return (
     <div className = "pb-4">
               <label htmlFor = {htmlFor} className="text-sm text-gray-500 block">
                 {label}
               </label>
               <input
-              placeholder = {placeholder} id = {id} type = {type}
+              placeholder = {placeholder} id = {id} type = {type} onChange = {onChange} value = {value}
               className = "border-2 text-sm placeholder-gray-300 focus:ring-0 focus:border-green-600 outline-none border-gray-500 p-2 rounded-md w-full"
               />
     </div>
